411403bed70d074c0559afb554cc54a71403bb1d
Crispin <c.read@whitefusemedia.com>
2014-01-08 11:25:26 +0000

    add site name to header

--- templates/layout/page-full-width.tpl.php
+++ templates/layout/page-full-width.tpl.php
@@ -5,6 +5,9 @@
 			<?php if ($logo): ?>
 				<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
 			<?php endif; ?>
+			<div id="site-name">
+			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+			</div>
 	<?php print render($page['header']); ?>
 		</div></div><!-- /#header -->
 
